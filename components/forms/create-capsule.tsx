8e7e886ba2a0e8098a56a4dd6c0111ede2862b85
olamide <olamideumarq@gmail.com>
2024-10-28 17:18:49 +0100

    feat: form component

---
+++ components/forms/create-capsule.tsx
@@ -0,0 +1,96 @@
+import { CustomInput } from "@/components/inputs/custom-input";
+import { CustomSelect } from "@/components/inputs/custom-select";
+import { CustomDatePicker } from "../inputs/date-picker";
+import { Formik, Form, FormikHelpers } from "formik";
+import { createCapsuleSchema } from "@/app/schemas";
+import { Button } from "@/components/ui/button";
+import { useAppDispatch, useAppSelector } from "@/app/lib/store/hooks";
+import { Capsule } from "@/app/lib/data";
+
+const statusOptions = [
+  { label: "Active", value: "active" },
+  { label: "Destroyed", value: "destroyed" },
+  { label: "Unknown", value: "unknown" },
+  { label: "Retired", value: "retired" },
+];
+const typeOptions = [
+  { label: "Dragon 1.0", value: "Dragon 1.0" },
+  { label: "Dragon 1.1", value: "Dragon 1.1" },
+  { label: "Dragon 2.0", value: "Dragon 2.0" },
+  { label: "Dragon 2.1", value: "Dragon 2.1" },
+];
+
+export interface Values {
+  original_launch: string;
+  status: string;
+  type: string;
+  missions: number;
+  capsule_serial: string;
+}
+
+export function CreateCapsuleForm({
+  onFinish,
+  initialValues,
+  handleAction,
+  action,
+}: {
+  onFinish: () => void;
+  initialValues: Values;
+  handleAction: (x: Values) => void;
+  action: string;
+}) {
+  const nextID = useAppSelector((state) => state.capsules.nextID);
+  const handleSubmit = (
+    values: Values,
+    { setSubmitting }: FormikHelpers<Values>,
+  ) => {
+    console.log(typeof values.original_launch);
+    handleAction(values);
+    onFinish();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={createCapsuleSchema}
+      onSubmit={handleSubmit}
+    >
+      {(props) => (
+        <Form className="flex flex-row gap-3 flex-wrap">
+          <CustomInput
+            label="missions"
+            type="number"
+            name="missions"
+            placeholder="Enter number of missions"
+          />
+          <CustomSelect
+            label="status"
+            options={statusOptions}
+            name="status"
+            placeholder="select status"
+          />
+          <CustomSelect
+            label="type"
+            options={typeOptions}
+            name="type"
+            placeholder="select type"
+          />
+          <CustomDatePicker
+            label="Original Launch Date"
+            name="original_launch"
+            placeholder="dd/mm/yy"
+          />
+
+          <div className="w-full flex items-end gap-4 flex-row">
+            <Button type="submit" className="capitalize">
+              {action} Capsule
+            </Button>
+            <Button type="button" variant="outline" onClick={onFinish}>
+              Cancel
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
